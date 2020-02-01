250961e29e0553aa98c16197984456370252f6ad
Patrick Böker <patzim@users.noreply.github.com>
2020-02-01 03:17:56 +0100

    Bring macos install script in line with the others

--- public/install-on-macos.sh
+++ public/install-on-macos.sh
@@ -3,15 +3,26 @@
 set -o errexit
 set -o pipefail
 
+for cmd in "rakudobrew", "p6env"; do
+    if which $cmd &>/dev/null; then
+        echo "A previous $cmd installation was found. rakubrew can not be used in"
+        echo "parallel with other Raku version managers."
+        echo "Please remove $cmd before installing rakubrew."
+        exit 1
+    fi
+done
+
 TMP_DIR=`mktemp -d -t 'rakubrew-temp-dir'`
+
+echo "Downloading rakubrew..."
 
 curl -s https://rakubrew.org/macos/rakubrew -o $TMP_DIR/rakubrew
 
 chmod +x $TMP_DIR/rakubrew
 : ${RAKUBREW_HOME:=$($TMP_DIR/rakubrew home)}
 
-if [ -e $RAKUBREW_HOME ]; then
-    echo "$RAKUBREW_HOME already exists!" 1>&2
+if [ -e $RAKUBREW_HOME/bin/rakubrew ]; then
+    echo "A previous rakubrew installation was found here: $RAKUBREW_HOME" 1>&2
     echo "You should just upgrade your installation instead of installing over it!" 1>&2
     echo "You can run" 1>&2
     echo "    rakubrew self-upgrade" 1>&2
@@ -21,10 +32,10 @@ if [ -e $RAKUBREW_HOME ]; then
     exit 1
 fi
 
-echo "Installing rakubrew to $RAKUBREW_HOME"
+echo "Installing rakubrew to $RAKUBREW_HOME ..."
+echo ""
 
 mkdir -p $RAKUBREW_HOME/bin
 mv $TMP_DIR/rakubrew $RAKUBREW_HOME/bin
 rmdir $TMP_DIR
 $RAKUBREW_HOME/bin/rakubrew init
-
